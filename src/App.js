5e94b58899bbaf95efe4c20515dca76a30fe47d0
masum691 <masumhu691@gmail.com>
2021-09-24 09:26:56 +0600

    set

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 
 // import { useState } from 'react';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 import Device from './components/Device/Device'
 
 
 function App() {
   const [steps, setSteps] = useState(0)
+  const handleWalk = () => {
+    setSteps(steps + 1)
+    // console.log(steps)
+  }
+  useEffect ( () => {
+    console.log(steps)
+  }, [steps])
   return (
     <div className="App">
       <Device name="samsung" price="22332" />
+      <h3>My Steps: {steps}</h3>
+      <button onClick={handleWalk}>Walk</button>
     </div>
   );
 }
 
-export default App; 
+export default App;
